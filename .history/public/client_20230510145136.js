e949fbb68e84d0828ca6132a5bb1184afcb08275
Marcelo Lucas <marcelo.lucas.eng@gmail.com>
2023-05-10 16:59:41 -0300

    Objetos salvos em arquivo json funcionando 2023-05-10

---
+++ .history/public/client_20230510145136.js
@@ -0,0 +1,119 @@
+const fs = require("fs");
+const socket = io();
+
+let previousObjects = [];
+
+// ouvinte de eventos para o botão "criar"
+document.getElementById("criar").addEventListener("click", function() {
+    const nome = document.getElementById("nome").value;
+    if (nome !== "") {
+      socket.emit("createObject", nome);
+      // solicita a lista atualizada de objetos do servidor
+      socket.emit("getObjects");
+    }
+    console.log("Objeto criado...");
+  });
+
+// função para criar objeto
+function createObject() {
+    var nome = document.getElementById("nome").value;
+    socket.emit("createObject", nome);
+  }
+
+// função para alterar status do objeto
+function changeStatus(objectId) {
+  socket.emit("changeStatus", objectId);
+}
+
+// função para excluir objeto
+function deleteObject(objectId) {
+  socket.emit("deleteObject", objectId);
+}
+
+// função para atualizar lista de objetos
+function updateObjectsList(objects) {
+  // limpa container de objetos
+  const objectsContainer = document.getElementById("objects-container");
+  objectsContainer.innerHTML = "";
+  // cria DIV para cada objeto e adiciona ao container
+  objects.forEach((object) => {
+    const objectDiv = document.createElement("div");
+    objectDiv.setAttribute("class", "object");
+    const objectIdSpan = document.createElement("span");
+    objectIdSpan.innerHTML = `ID: ${object.id}`;
+    const objectStatusSpan = document.createElement("span");
+    objectStatusSpan.innerHTML = `Status: ${object.status}`;
+    const objectButton = document.createElement("button");
+    objectButton.setAttribute("onclick", `changeStatus(${object.id})`);
+    objectButton.innerHTML = (object.status == "Ativo") ? "Desativar" : "Ativar";
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("onclick", `deleteObject(${object.id})`);
+    deleteButton.innerHTML = "Excluir";
+    // adiciona elementos à DIV do objeto
+    objectDiv.appendChild(objectIdSpan);
+    objectDiv.appendChild(objectStatusSpan);
+    objectDiv.appendChild(objectButton);
+    objectDiv.appendChild(deleteButton);
+    // adiciona DIV do objeto ao container
+    objectsContainer.appendChild(objectDiv);
+  });
+}
+
+// recebe atualização da lista de objetos do servidor
+socket.on("objectsList", (objects) => {
+  updateObjectsList(objects);
+  // inicia a monitoração de atualização de objetos
+  monitorObjectsUpdate(objects); 
+});
+
+// função para monitorar a atualização de objetos
+function monitorObjectsUpdate(objects) {
+  setInterval(() => {
+//    if (JSON.stringify(objects) !== JSON.stringify(previousObjects)) {
+//      console.log("A variável objects foi atualizada.");
+//      previousObjects = objects;
+//   }
+//  }, 1000); // verifica a cada 1 segundo
+//}
+
+// atualiza lista de objetos quando a página é carregada
+window.onload = () => {
+    socket.emit("getObjects");
+  };
+
+// cliente conectado ao servidor: visto do lado do cliente
+socket.on("connect", () => {
+  console.log("Conectado ao servidor!");
+});
+
+// cliente desconectado do servidor: visto do lado do cliente
+socket.on("disconnect", () => {
+  console.log("Desconectado do servidor!");
+});
+
+
+function monitorObjectsUpdate(objects) {
+  setInterval(() => {
+    if (JSON.stringify(objects) !== JSON.stringify(previousObjects)) {
+      console.log("A variável objects foi atualizada.");
+      previousObjects = objects;
+
+      // verifica se o arquivo já existe
+      if (fs.existsSync("objetos.json")) {
+        try {
+          // escreve o objeto em um arquivo JSON
+          fs.writeFileSync("objetos.json", JSON.stringify(previousObjects));
+        } catch (err) {
+          console.error("Erro ao gravar objeto no arquivo JSON:", err);
+        }
+      } else {
+        try {
+          // cria um novo arquivo e grava o objeto JSON nele
+          fs.writeFileSync("objetos.json", JSON.stringify(previousObjects));
+        } catch (err) {
+          console.error("Erro ao criar e gravar objeto no arquivo JSON:", err);
+        }
+      }
+    }
+  }, 1000); // verifica a cada 1 segundo
+}
---
+++ .history/server_20230510162919.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
+const fs = require("fs");
+
+const PORT = 3000;
+
+// coleção de objetos
+let objects = [];
+
+// define a rota para o arquivo index.html
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+
+// define a rota para o arquivo client.js
+app.get('/client.js', function (req, res) {
+    res.sendFile(__dirname + '/public/client.js');
+});
+
+// define a rota para o arquivo style.css
+app.get('/style.css', function (req, res) {
+    res.type('text/css');
+    res.sendFile(__dirname + '/public/style/style.css');
+});
+
+// define a rota para o arquivo favicon.ico
+app.get('/favicon.ico', function (req, res) {
+    res.sendFile(__dirname + '/public/favicon.ico');
+});
+
+// função para enviar a lista de objetos para todos os clientes
+function sendObjectsList() {
+  io.emit("objectsList", objects);
+}
+
+// função para criar um novo objeto
+function createObject() {
+  const newObject = {
+    id: objects.length,
+    status: "Inativo"
+  };
+  objects.push(newObject);
+  sendObjectsList();
+}
+
+// função para alterar o status de um objeto
+function changeStatus(objectId) {
+  const object = objects.find(obj => obj.id == objectId);
+  if (object) {
+    object.status = (object.status == "Ativo") ? "Inativo" : "Ativo";
+    sendObjectsList();
+  }
+}
+
+// função para excluir um objeto
+function deleteObject(objectId) {
+  objects = objects.filter(obj => obj.id != objectId);
+  sendObjectsList();
+}
+
+// evento de conexão do socket.io
+io.on("connection", (socket) => {
+  // envia a lista de objetos ao cliente quando ele se conecta
+  socket.emit("objectsList", objects);
+
+  // cria um novo objeto quando o cliente emite o evento "createObject"
+  socket.on("createObject", () => {
+    createObject();
+  });
+
+  // altera o status do objeto quando o cliente emite o evento "changeStatus"
+  socket.on("changeStatus", (objectId) => {
+    changeStatus(objectId);
+  });
+
+  // exclui o objeto quando o cliente emite o evento "deleteObject"
+  socket.on("deleteObject", (objectId) => {
+    deleteObject(objectId);
+  });
+
+  // cliente conectado ao servidor: visto do lado do servidor
+  console.log("Cliente conectado!");
+  
+  // cliente foi desconectado do servidor: visto do lado do servidor
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado!");
+  });
+});
+
+// função para salvar/atualizar o arquivo objetos.json
+function saveObjects() {
+  const json = JSON.stringify(objects);
+  fs.writeFile('objetos.json', json, (err) => {
+    if (err) {
+      console.error("Erro ao atualizar o arquivo objetos.json:", err);
+    } else {
+      console.log("Arquivo objetos.json atualizado com sucesso.");
+    }
+  });
+
+}
+
+// inicia o servidor na porta PORT (3000)
+http.listen(PORT, () => {
+
+  if (fs.existsSync("objetos.json")) {
+    const data = fs.readFileSync("objetos.json");
+    objects = JSON.parse(data);
+  }
+
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+// sempre que o objeto objects for alterado, chamamos a função saveObjects
+objects.push({ newObject: 'newValue' });
+saveObjects();
